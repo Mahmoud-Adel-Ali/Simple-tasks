b0e3d62f11403790b346f8f78e7a4a4c9de494ed
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-10-12 18:31:12 -0700

    flutter localization done

--- flutter_localization/lib/main.dart
+++ flutter_localization/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localization/generated/l10n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const FluttterLocalization());
@@ -16,18 +19,44 @@ class FluttterLocalization extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      locale: const Locale('ar'),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       home: const Home(),
     );
   }
 }
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text(S.of(context).title)),
+      body: Center(
+        child: Row(
+          children: [
+            Text(S.of(context).hello),
+            Padding(
+              padding:  EdgeInsets.only(
+                left: isArabic() ? 0:16,
+                right: isArabic()? 16:0,
+              ),
+              child: Text(S.of(context).name),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  bool isArabic() {
+    return Intl.getCurrentLocale() == 'ar';
   }
 }
-
-
-
